14f488f760b2c377d8c33ba906ec78e70d61e7d0
Tor12378 <87545113+Tor12378@users.noreply.github.com>
2021-11-08 16:22:37 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,71 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+int rec(void) {
+    int e1, e2;
+    int num;
 
-static int days_in_month[] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-int day, month, year;
-
-unsigned short day_counter;
-
-int is_leap(int y) {
-    return ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0);
-}
-
-next_day()
-{
-    day += 1; day_counter++;
-    if (day > days_in_month[month]) {
-        day = 1;
-        month += 1;
-        if (month > 12) {
-            month = 1;
-            year += 1;
-            if (is_leap(year)) {
-                days_in_month[2] = 29;
-            } else {
-                days_in_month[2] = 28;
-            }
+    char symbol;
+    scanf("%s", &symbol);
+    if (symbol != '*' && symbol != '/') {
+        num = atoi(&symbol);
+        return num;
+    } else {
+        if (symbol == '*') {
+            e1 = rec();
+            e2 = rec();
+            return e1 * e2;
+        } else {
+            e1 = rec();
+            e2 = rec();
+            return e1 / e2;
         }
     }
 }
 
-set_date(int d, int m, int y)
-{
-    m < 1 ? m = 1 : 0;
-    m > 12 ? m = 12 : 0;
-    d < 1 ? d = 1 : 0;
-    d > days_in_month[m] ? d = days_in_month[m] : 0;
-    if (is_leap(y)){
-        days_in_month[2] = 29;
-    } else {
-        days_in_month[2] = 28;
-    }
-    day = d;
-    month = m;
-    year = y;
-}
+int main(void) {
 
-skip_days(int x)
-{
-    int i;
-    for (i=0;i<x;i++) next_day();
-}
-
-print_date()
-{
-    printf ("%d %d %d", day, month, year);
-}
-
-int main(int argc, char **argv)
-{
-    int d,m,y;
-    scanf("%d/%d/%d",&d,&m,&y);
-
-    set_date(d,m,y);
-
-    skip_days(1);
-    day_counter = 0;
-    
-
-    print_date();
+    printf("%d", rec());
 
     return 0;
 }
